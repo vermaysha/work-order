dbc3c4ec4d65cfa08949bb9229d36d28f20ffa14
Ashary Vermaysha <asharyver13@gmail.com>
2021-01-19 12:37:44 +0700

    perbaiki di kolom Detail Wo

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Request')
+@section('title', 'Detail Request: ' . $order->title)
 
 @section('breadcrumb')
 <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
@@ -17,78 +17,23 @@
                 <div class="card card-primary card-outline">
                     <div class="card-header">Detail</div>
                     <div class="card-body">
-                        <table class="table">
-                            <tr>
-                                <th class="col-md-3">Nomor</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->wo_number }}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Judul</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->title }}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Kategori</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">
-                                    @if ($order->category == 'technical')
-                                    <span class="badge bg-indigo">Teknis</span>
-                                    @else
-                                    <span class="badge bg-maroon">Non Teknis</span>
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Status</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">
-                                    @if ($order->status == 'open')
-                                        <span class="badge bg-primary">Terbuka</span>
-                                    @elseif ($order->status == 'progress')
-                                        <span class="badge bg-success">Proses</span>
-                                    @elseif ($order->status == 'finish')
-                                        <span class="badge bg-warning">Selesai</span>
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Kepada</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->assign->fullname }}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Dari</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->from->fullname }}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Deskripsi</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{!! $order->description !!}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Dibuat pada</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->created_at->format('l, d F Y') }}</td>
-                            </tr>
-                            <tr>
-                                <th class="col-md-3">Terakhir diubah pada</th>
-                                <td class="col-md-1">:</td>
-                                <td class="col-md-8">{{ $order->updated_at->format('l, d F Y') }}</td>
-                            </tr>
-                        </table>
-                        {{-- <div class="row">
-                            <div class="col-md-3">
-                                <strong>Judul:</strong>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Nomor: </label>
                             </div>
-                            <div class="col-md-9">{{ $order->title }}</div>
+                            <div class="col-md-10">{{ $order->wo_number }}</div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <strong>Kategori:</strong>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Judul: </label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-10">{{ $order->title }}</div>
+                        </div>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Kategori: </label>
+                            </div>
+                            <div class="col-md-10">
                                 @if ($order->category == 'technical')
                                 <span class="badge bg-indigo">Teknis</span>
                                 @else
@@ -96,12 +41,51 @@
                                 @endif
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <strong>Judul:</strong>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Status: </label>
                             </div>
-                            <div class="col-md-9">{{ $order->title }}</div>
-                        </div> --}}
+                            <div class="col-md-10">
+                                @if ($order->status == 'open')
+                                    <span class="badge bg-primary">Terbuka</span>
+                                @elseif ($order->status == 'progress')
+                                    <span class="badge bg-success">Proses</span>
+                                @elseif ($order->status == 'finish')
+                                    <span class="badge bg-warning">Selesai</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Kepada: </label>
+                            </div>
+                            <div class="col-md-10">{{ $order->assign->fullname }}</div>
+                        </div>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Dari: </label>
+                            </div>
+                            <div class="col-md-10">{{ $order->from->fullname }}</div>
+                        </div>
+                        <div class="row border-bottom mb-2 pb-2">
+                            <div class="col-md-2">
+                                <label>Deskripsi: </label>
+                            </div>
+                            <div class="col-md-10">
+                                {!! $order->description !!}</div>
+                        </div>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Dibuat pada: </label>
+                            </div>
+                            <div class="col-md-10">{{ $order->created_at->format('H:i:s - l, d F Y') }}</div>
+                        </div>
+                        <div class="row border-bottom mb-2">
+                            <div class="col-md-2">
+                                <label>Terakhir diubah pada: </label>
+                            </div>
+                            <div class="col-md-10">{{ $order->updated_at->format('H:i:s - l, d F Y') }}</div>
+                        </div>
                     </div>
                 </div>
             </div>
